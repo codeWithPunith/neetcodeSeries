7e6da7309da45b44416ad177798114837e384459
codeWithPunith <codewithpunith@gmail.com>
2025-08-15 23:19:50 +0530

    Day2:Solvong two sum

---
+++ ArraysAndHashmap/Day2/problem3.java
@@ -0,0 +1,23 @@
+/**Two sum
+ * Easy
+ * Logic just store the values in hashmap and check for the needed value for the 
+ *sum in the hashmap which is O(1)
+ */
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> prevMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+
+            if (prevMap.containsKey(diff)) {
+                return new int[] { prevMap.get(diff), i };
+            }
+
+            prevMap.put(num, i);
+        }
+
+        return new int[] {};
+    }
+}
