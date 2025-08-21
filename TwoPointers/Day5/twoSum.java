7e8e6bf8cb8cb8c61d2bceb54d247c383bca6b28
codeWithPunith <codewithpunith@gmail.com>
2025-08-21 23:47:20 +0530

    Two sum n palindrome

--- TwoPointers/Day5/twoSum.java
+++ TwoPointers/Day5/twoSum.java
@@ -5,10 +5,8 @@ if the sum is less move right else left
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int l = 0, r = numbers.length - 1;
-
         while (l < r) {
             int curSum = numbers[l] + numbers[r];
-
             if (curSum > target) {
                 r--;
             } else if (curSum < target) {
@@ -19,4 +17,5 @@ public class Solution {
         }
         return new int[0];
     }
+
 }
