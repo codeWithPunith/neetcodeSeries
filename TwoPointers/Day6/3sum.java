fa6a4a03d931dd843622d294e5ad4ed9c9a8e5a5
codeWithPunith <codewithpunith@gmail.com>
2025-08-24 00:41:32 +0530

    Solving three sum a little tricky

--- TwoPointers/Day6/3sum.java
+++ TwoPointers/Day6/3sum.java
@@ -1,3 +1,35 @@
 /*3sum
-Logic
+Logic:first sort the array and then run a loop for the first element and try to skip
+the same element by using a if statement .And now its basically a two pointer problem 
+ex:if u get a element -3 then by using two pointers u need to find two sum equating to 
++3
  */
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum > 0) {
+                    r--;
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    l++;
+                    r--;
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
